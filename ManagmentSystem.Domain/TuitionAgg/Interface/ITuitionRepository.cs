977ffdf6ac5d1358c0cd5fccaf5366152bc25abc
saeedkamyab <76224606+saeedkamyab@users.noreply.github.com>
2024-01-13 13:51:18 +0330

    I made base repository and my entity repository

---
+++ ManagmentSystem.Domain/TuitionAgg/Interface/ITuitionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroFramework.Domain.BaseInterface;
+
+namespace ManagmentSystem.Domain.TuitionAgg.Interface
+{
+    public interface ITuitionRepository:IBaseRepository<long,Tuition>
+    {
+
+    }
+}
---
+++ ZeroFramework/Domain/BaseInterface/IBaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroFramework.Domain.BaseInterface
+{
+    public interface IBaseRepository<TKey, TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// I made this repository base to avoid reptation some actions that they are routine.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        
+        TEntity Get(TKey Id);
+
+        List<TEntity> GetAll();
+
+        void Create(TKey entity);
+
+        void Update(TEntity entity);
+
+        bool Delete(TEntity entity);
+
+        bool DeleteRange(List<TEntity> entities);
+
+        bool Exists(Expression<Func<TKey, bool>> expression);
+      
+        void SaveChanges();
+ 
+    }
+}
