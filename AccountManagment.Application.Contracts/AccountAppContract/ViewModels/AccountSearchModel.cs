5d8cfcd4099faa048b6830b35934e0abf3de86b6
saeedkamyab <76224606+saeedkamyab@users.noreply.github.com>
2024-02-21 15:33:16 +0330

    finished serach part in add to termclass

--- AccountManagment.Application.Contracts/AccountAppContract/ViewModels/AccountSearchModel.cs
+++ AccountManagment.Application.Contracts/AccountAppContract/ViewModels/AccountSearchModel.cs
@@ -1,7 +1,10 @@
-﻿namespace AccountManagment.Application.Contracts.AccountAppContract.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountManagment.Application.Contracts.AccountAppContract.ViewModels
 {
     public class AccountSearchModel
     {
+        [Required(ErrorMessage ="برای جستجو باید نام را وارد نمایید")]
         public string Fullname { get; set; }
 
         public string Username { get; set; }
